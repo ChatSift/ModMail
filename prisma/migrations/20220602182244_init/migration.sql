c220da22a90318f8a91e51d7dacb9c97be454a39
didinele <didinele.dev@gmail.com>
2022-06-02 21:23:26 +0300

    feat: prisma modmail snippet models

---
+++ prisma/migrations/20220602182244_init/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "SnippetUpdates" (
+    "snippetUpdateId" SERIAL NOT NULL,
+    "snippetId" INTEGER NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedBy" TEXT NOT NULL,
+    "oldContent" TEXT NOT NULL,
+
+    CONSTRAINT "SnippetUpdates_pkey" PRIMARY KEY ("snippetUpdateId")
+);
+
+-- CreateTable
+CREATE TABLE "Snippet" (
+    "snippetId" SERIAL NOT NULL,
+    "guildId" TEXT NOT NULL,
+    "createdById" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "lastUpdatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Snippet_pkey" PRIMARY KEY ("snippetId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Snippet_guildId_name_key" ON "Snippet"("guildId", "name");
+
+-- AddForeignKey
+ALTER TABLE "SnippetUpdates" ADD CONSTRAINT "SnippetUpdates_snippetId_fkey" FOREIGN KEY ("snippetId") REFERENCES "Snippet"("snippetId") ON DELETE CASCADE ON UPDATE CASCADE;
