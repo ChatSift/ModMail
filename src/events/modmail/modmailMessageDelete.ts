d0745dfd048247327ed2d7221d772deb9e808186
didinele <didinele.dev@gmail.com>
2022-07-03 23:01:20 +0300

    feat: message delete tracking

---
+++ src/events/modmail/modmailMessageDelete.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from '@prisma/client';
+import { Client, Events, Message, PartialMessage, ThreadChannel } from 'discord.js';
+import { singleton } from 'tsyringe';
+import type { Event } from '#struct/Event';
+
+@singleton()
+export default class implements Event<typeof Events.MessageDelete> {
+	public readonly name = Events.MessageDelete;
+
+	public constructor(private readonly prisma: PrismaClient, private readonly client: Client<true>) {}
+
+	public async handle(message: Message | PartialMessage) {
+		if (message.inGuild() || message.author?.bot) {
+			return;
+		}
+
+		const threadMessage = await this.prisma.threadMessage.findFirst({
+			where: { userMessageId: message.id },
+			include: { thread: true },
+		});
+		if (!threadMessage || threadMessage.thread.closedAt) {
+			return;
+		}
+
+		const guild = this.client.guilds.cache.get(threadMessage.guildId);
+		const channel = (await guild?.channels
+			.fetch(threadMessage.thread.channelId)
+			.catch(() => null)) as ThreadChannel | null;
+
+		if (!channel) {
+			return;
+		}
+
+		const existing = await channel.messages.fetch(threadMessage.guildMessageId);
+		await channel.send(`User deleted their message: <${existing.url}>`);
+	}
+}
