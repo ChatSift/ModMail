f60da7a8d027b18bec657c77289fbd1e3fe96996
didinele <didinele.dev@gmail.com>
2022-07-13 18:52:17 +0300

    fix(getSortedMemberRolesString): deal with no roles

--- packages/bot/src/util/getSortedMemberRoles.ts
+++ packages/bot/src/util/getSortedMemberRoles.ts
@@ -1,6 +1,10 @@
 import type { GuildMember } from 'discord.js';
 
 export function getSortedMemberRolesString(member: GuildMember): string {
+	if (!member.roles.cache.size) {
+		return 'none';
+	}
+
 	return member.roles.cache
 		.filter((r) => r.id !== member.guild.id)
 		.sort((a, b) => b.position - a.position)
