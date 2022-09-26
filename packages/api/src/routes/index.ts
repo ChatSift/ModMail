5b2b2e7a22cbd746e4ac49c29176e5fbc73dea60
Sazzo <felipe@sazz.dev>
2022-09-26 15:37:16 -0300

    feat(api): add update snippet endpoint

--- packages/api/src/routes/index.ts
+++ packages/api/src/routes/index.ts
@@ -2,3 +2,4 @@ export { default as GetSettingsRoute } from './getSettings';
 export { default as UpdateSettingsRoute } from './updateSettings';
 export { default as GetSnippetsRoute } from './getSnippets';
 export { default as GetSnippetRoute } from './getSnippet';
+export { default as UpdateSnippetRoute } from './updateSnippet';
