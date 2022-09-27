ee143fe827ed0772f375dfb432b3c4f3c10293af
Sazzo <felipe@sazz.dev>
2022-09-27 17:54:33 -0300

    fix(api/deleteSnippet): remove unnecessary body schema

--- packages/api/src/routes/deleteSnippet.ts
+++ packages/api/src/routes/deleteSnippet.ts
@@ -6,27 +6,18 @@ import { PrismaClient } from '@prisma/client';
 import type { BaseValidator, InferType } from '@sapphire/shapeshift';
 import { s } from '@sapphire/shapeshift';
 import { Routes } from 'discord-api-types/v10';
-import type { NextHandler, Response } from 'polka';
+import type { NextHandler, Request, Response } from 'polka';
 import { singleton } from 'tsyringe';
 import { Env } from '../util/env';
 import type { Snippet } from '../util/models';
 import { snowflakeSchema } from '../util/snowflakeSchema';
 
-const schema = s.object({
-	name: s.string.lengthGreaterThan(0).lengthLessThanOrEqual(32).optional,
-	content: s.string.lengthGreaterThan(0).lengthLessThanOrEqual(1_900).optional,
-	updatedById: snowflakeSchema,
-}).strict;
-type Body = InferType<typeof schema>;
-
 @singleton()
-export default class extends Route<Snippet, Body> {
+export default class extends Route<Snippet, never> {
 	public info = {
 		method: RouteMethod.delete,
 		path: '/modmail/v1/guilds/:guildId/snippets/:snippetId',
 	} as const;
-
-	public override readonly bodyValidationSchema: BaseValidator<Body> = schema;
 
 	public constructor(
 		private readonly prisma: PrismaClient,
@@ -36,7 +27,7 @@ export default class extends Route<Snippet, Body> {
 		super();
 	}
 
-	public async handle(req: TRequest<Body>, res: Response, next: NextHandler) {
+	public async handle(req: Request, res: Response, next: NextHandler) {
 		const { guildId, snippetId } = req.params as { guildId: string; snippetId: string };
 
 		const snippetIdNum = Number.parseInt(snippetId, 10);
