06433d519cecbfa0992421360724ffce440dd912
Felipe Garcia <felipe@sazz.dev>
2022-11-29 14:07:13 -0300

    fix(api): export createSnippet route

--- packages/api/src/routes/index.ts
+++ packages/api/src/routes/index.ts
@@ -2,5 +2,6 @@ export { default as GetSettingsRoute } from './getSettings';
 export { default as UpdateSettingsRoute } from './updateSettings';
 export { default as GetSnippetsRoute } from './getSnippets';
 export { default as GetSnippetRoute } from './getSnippet';
+export { default as CreateSnippetRoute} from "./createSnippet";
 export { default as UpdateSnippetRoute } from './updateSnippet';
 export { default as DeleteSnippetRoute } from './deleteSnippet';
