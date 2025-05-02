df5ecc365fc6fe051061a856f9be4bd8539cffd3
didinele <didinele.dev@gmail.com>
2025-05-02 12:01:21 +0300

    feat: significant cache hit improvements

--- packages/bot/src/util/getUserGuilds.ts
+++ packages/bot/src/util/getUserGuilds.ts
@@ -1,3 +1,4 @@
+import { setTimeout } from 'node:timers';
 import { PrismaClient } from '@prisma/client';
 import type { Guild, Snowflake } from 'discord.js';
 import { Client, Collection } from 'discord.js';
@@ -5,6 +6,7 @@ import { container } from 'tsyringe';
 import { logger } from './logger.js';
 
 export const ENABLED_CACHED_GUILDS = new Map<Snowflake, boolean>();
+const NOT_IN_GUILD_CACHE = new Map<`${Snowflake}-${Snowflake}`, boolean>();
 
 export async function getUserGuilds(userId: string): Promise<Collection<string, Guild>> {
 	const client = container.resolve(Client);
@@ -14,24 +16,36 @@ export async function getUserGuilds(userId: string): Promise<Collection<string, 
 	logger.debug({ userId, enabledCacheSize: ENABLED_CACHED_GUILDS.size, cachedInGuilds }, 'Fetching guilds for user');
 
 	const results = await Promise.all(
-		Array.from(client.guilds.cache.values(), async (guild) =>
-			guild.members
-				.fetch(userId)
-				.then(async () => {
-					if (ENABLED_CACHED_GUILDS.get(guild.id)) {
-						return [guild.id, guild];
-					}
+		Array.from(
+			client.guilds.cache.filter((guild) => !NOT_IN_GUILD_CACHE.has(`${userId}-${guild.id}`)).values(),
+			async (guild) =>
+				guild.members
+					.fetch(userId)
+					.then(async () => {
+						if (ENABLED_CACHED_GUILDS.get(guild.id)) {
+							return [guild.id, guild];
+						}
 
-					const settings = await prisma.guildSettings.findFirst({ where: { guildId: guild.id } });
-					if (settings?.modmailChannelId) {
-						ENABLED_CACHED_GUILDS.set(guild.id, true);
-						return [guild.id, guild];
-					}
+						const settings = await prisma.guildSettings.findFirst({ where: { guildId: guild.id } });
+						if (settings?.modmailChannelId) {
+							ENABLED_CACHED_GUILDS.set(guild.id, true);
+							return [guild.id, guild];
+						}
 
-					ENABLED_CACHED_GUILDS.set(guild.id, false);
-					return null;
-				})
-				.catch(() => null),
+						ENABLED_CACHED_GUILDS.set(guild.id, false);
+						return null;
+					})
+					.catch(() => {
+						NOT_IN_GUILD_CACHE.set(`${userId}-${guild.id}`, true);
+						setTimeout(
+							() => {
+								NOT_IN_GUILD_CACHE.delete(`${userId}-${guild.id}`);
+							},
+							10 * 60 * 1_000,
+						).unref();
+
+						return null;
+					}),
 		),
 	);
 
