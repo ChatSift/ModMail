5bf07f2749d81c8f8a88377ecf52383a21db8417
didinele <didinele.dev@gmail.com>
2025-05-02 11:49:58 +0300

    chore: insightful debug logging

--- packages/bot/src/util/getUserGuilds.ts
+++ packages/bot/src/util/getUserGuilds.ts
@@ -2,12 +2,16 @@ import { PrismaClient } from '@prisma/client';
 import type { Guild, Snowflake } from 'discord.js';
 import { Client, Collection } from 'discord.js';
 import { container } from 'tsyringe';
+import { logger } from './logger.js';
 
 export const ENABLED_CACHED_GUILDS = new Map<Snowflake, boolean>();
 
 export async function getUserGuilds(userId: string): Promise<Collection<string, Guild>> {
 	const client = container.resolve(Client);
 	const prisma = container.resolve(PrismaClient);
+
+	const cachedInGuilds = client.guilds.cache.reduce((acc, guild) => acc + (guild.members.cache.has(userId) ? 1 : 0), 0);
+	logger.debug({ userId, enabledCacheSize: ENABLED_CACHED_GUILDS.size, cachedInGuilds }, 'Fetching guilds for user');
 
 	const results = await Promise.all(
 		Array.from(client.guilds.cache.values(), async (guild) =>
